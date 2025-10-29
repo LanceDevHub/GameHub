771d95f897a98d826b6a584a4838d69020649562
“lemter” <lemter@students.uni-mainz.de>
2025-10-30 00:12:54 +0100

    Implement Dark Mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, Text, Box } from "@chakra-ui/react";
 import logo from "../assets/gamehub_logo.webp";
+import { useColorMode } from "@/components/ui/color-mode";
 
 const NavBar = () => {
+  const { toggleColorMode } = useColorMode();
+
   return (
     <HStack>
       <Image src={logo} boxSize="80px"></Image>
       <Text>NavBar</Text>
+      <Button onClick={toggleColorMode}>Toggle Mode</Button>
     </HStack>
   );
 };
