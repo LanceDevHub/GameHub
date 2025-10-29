7056982c539bec9716bd04b00bcbdaa0c95d7229
“lemter” <lemter@students.uni-mainz.de>
2025-10-29 01:58:09 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  return <Button variant="subtle">Subtle</Button>;
+  // Checks if we on "bigger devices" -> lg or "mobile" -> base
+  const isLg = useBreakpointValue({ base: false, lg: true }); // true if lg or above
+
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={isLg}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
